338d602ae2478f549b7d709dbb2bfeec838af60a
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2023-09-21 16:09:37 +1000

    initial commit

---
+++ serverSide/src/config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+     // port env
+     port:process.env.PORT,
+
+     // database env
+
+
+
+     //auth env
+
+
+
+
+
+
+
+}
--- serverSide/src/middleware/apiErrorHandler.js
+++ serverSide/src/middleware/apiErrorHandler.js
@@ -1,18 +1,19 @@
-const ApiError = require("./utils/ApiError");
+const ApiError = require('../utils/ApiError');
 
-function apiErrorHandler(err, req, res, next) {
-  // known error
-  if (err instanceof ApiError) {
-    res.status(err.code).json(err.message);
-    return;
+function apiErrorHandler(err,req,res,next) {
+     // known error
+     if(err instanceof ApiError){
+          res.status(err.code).json(err.message);
+          return;
+    
+     // unknown error 
+     }else {
+               console.error(err);
+               res.status(500).json({message:'Something went wrong, Please try again later'});
+          }
 
-    // unknown error
-  } else {
-    console.error(err);
-    res
-      .status(500)
-      .json({ message: "Something went wrong, Please try again later" });
-  }
+
 }
+
 
 module.exports = apiErrorHandler;
