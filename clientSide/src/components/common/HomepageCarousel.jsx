093e5203ac8738f694aa813eb14c46dbd9878a94
lei wang <114329129+coolkidsclubvip@users.noreply.github.com>
2024-03-06 00:38:43 +1100

    brand new carousel!

--- clientSide/src/components/common/HomepageCarousel.jsx
+++ clientSide/src/components/common/HomepageCarousel.jsx
@@ -4,18 +4,17 @@
 import Carousel from "react-bootstrap/Carousel";
 import BannerButton from "./BannerButton";
 import { Link } from "react-router-dom";
-// import ExampleCarouselImage from "components/ExampleCarouselImage";
 
 import * as styles from "../../styles/components/HomepageCarousel.css";
 
 function HomepageCarousel() {
   return (
     <div className={styles.carouselContainer}>
-      <Carousel fade>
+      <Carousel fade variant="dark">
         <Carousel.Item>
           <div className={styles.image1}>
             <div className={styles.carouselCaption}>
-              Sylinn's Lucent LED Mirror Range
+              Lucent LED Mirror Range
             </div>
             <div className={styles.carouselText}>
               Discover Sylinn's brand new Lucent LED Mirror Range, offering
@@ -24,28 +23,44 @@ function HomepageCarousel() {
             </div>
             <div className={styles.carouselButton}>
               <Link to="/products/led">
-              <BannerButton>EXPLORE</BannerButton>
+                <BannerButton>EXPLORE</BannerButton>
               </Link>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
-          <div className={styles.image1}>
+          <div className={styles.image2}>
             <div className={styles.carouselCaption}>
-              Sylinn's Lucent LED Mirror Range
+              Felicity Corner Bath Range
             </div>
             <div className={styles.carouselText}>
-              Discover Sylinn's brand new Lucent LED Mirror Range, offering
-              modern and efficient lighting solutions to enhance your everyday
-              beauty routines with comfort and convenience.
+              Introducing Sylinn's Felicity Corner Bath, available in 1400mm and
+              1600mm sizes, catering to unique customer needs with unparalleled
+              versatility and style.
             </div>
-            <button className={styles.carouselButton}>Explore Lucent</button>
+            <div className={styles.carouselButton}>
+              <Link to="/products/bath/felicity">
+                <BannerButton>EXPLORE</BannerButton>
+              </Link>
+            </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
           <div className={styles.image3}>
-            <div className={styles.carouselCaption}>第三张图的文字</div>
-            <button className={styles.carouselButton}>Explore Products </button>
+            <div className={styles.carouselCaption}>
+              Multi Colours Towel Rack Range
+            </div>
+            <div className={styles.carouselText}>
+              Discover our Australian Standard Heated Towel Rack, featuring 6
+              radiant colors to complement any bathroom décor. Crafted with
+              Grade 304 Polished Stainless Steel, it offers both style and
+              functionality for your daily routine.
+            </div>
+            <div className={styles.carouselButton}>
+              <Link to="/products/htr">
+                <BannerButton>EXPLORE</BannerButton>
+              </Link>
+            </div>
           </div>
         </Carousel.Item>
       </Carousel>
--- clientSide/src/styles/components/HomepageCarousel.css.ts
+++ clientSide/src/styles/components/HomepageCarousel.css.ts
@@ -17,71 +17,121 @@ export const image1 = style({
   backgroundSize: "cover",
   // backgroundPositionY:"10%",
 
-  "@media screen and (max-width: 768px)": { backgroundPositionX: "90%" },
+  "@media screen and (max-width: 768px)": { backgroundPositionX: "80%" },
 });
 
 export const image2 = style({
   width: "100vw",
   height: "100vh",
-  backgroundImage: "url(/images/carousel_images/s001.webp)",
+  backgroundImage: "url(/images/carousel_images/s002.webp)",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
-  backgroundPositionY: "50%",
-  "@media screen and (max-width: 768px)": { backgroundPositionY: "40%" },
+  // backgroundPositionY:"10%",
+
+  "@media screen and (max-width: 768px)": { backgroundPositionX: "80%" },
 });
 
 export const image3 = style({
   width: "100vw",
   height: "100vh",
-  backgroundImage: "url(/images/carousel_images/s003.jpeg)",
+  backgroundImage: "url(/images/carousel_images/s003.webp)",
   backgroundRepeat: "no-repeat",
   backgroundSize: "cover",
   // backgroundPositionY: "50%",
-  "@media screen and (max-width: 768px)": { backgroundPositionX: "40%" },
+  "@media screen and (max-width: 768px)": { backgroundPositionX: "80%" },
 });
 
 // Carousel captions style
 export const carouselCaption = style({
   position: "absolute",
-  bottom: "60%", // 根据需要调整
+  bottom: "60%",
   left: "25%",
   transform: "translateX(-50%)",
   textAlign: "center",
   textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
-  color: vars.colors.white,
-  fontSize: vars.fontSizes["5x"],
+  color: vars.colors.black,
+  fontSize: vars.fontSizes["6x"],
   fontWeight: vars.fontWeights.bold,
   // transition: "all 1s ease-out",
   // ":hover": {
   //   color: vars.colors.black,
   // },
+  "@media screen and (max-width: 576px)": {
+    width: "100vw",
+    bottom: "50%",
+    left: "50%",
+    transform: "translateX(-50%)",
+  },
 
-  "@media screen and (max-width: 768px)": {
+  "@media screen and (min-width: 576px) and (max-width: 768px)": {
     bottom: "60%", // 视口变小时调整位置
     left: "50%",
     transform: "translateX(-50%)",
     fontSize: vars.fonts["x1"], // 可能需要调整字体大小
   },
-});
+  "@media screen and (min-width: 768px) and (max-width: 992px)": {
+    left: "50%",
+    transform: "translateX(-50%)",
+    bottom: "60%",
+  },
 
+  "@media screen and (min-width: 992px) and (max-width: 1200px)": {
+    width: "70vw",
+    left: "50%",
+    transform: "translateX(-50%)",
+    top: "30%",
+  },
+
+  "@media screen and (min-width: 1200px)": {
+    top: "30%",
+  },
+});
 
 export const carouselText = style({
   position: "absolute",
   width: "30%",
-  bottom: "40%", // 根据需要调整
+  // bottom: "40%", // 根据需要调整
   left: "25%",
   transform: "translateX(-50%)",
-  textAlign:"justify",
+  textAlign: "justify",
   // textShadow: "2px 2px 5px rgba(0, 0, 0, 0.5)",
   color: "black",
   fontSize: vars.fontSizes["3x"],
   fontWeight: vars.fontWeights.bold,
-  "@media screen and (max-width: 768px)": {
+
+  "@media screen and (max-width: 576px)": {
+    width: "100vw",
+    bottom: "20%",
+    left: "50%",
+    transform: "translateX(-50%)",
+    fontSize: vars.fontSizes["2x"],
+  },
+
+  "@media screen and (min-width: 576px) and (max-width: 768px)": {
     bottom: "30%", // 视口变小时调整位置
     left: "50%",
     transform: "translateX(-50%)",
     width: "90%",
-    fontSize: vars.fonts["x1"], // 可能需要调整字体大小
+    fontSize: vars.fonts["x1"],
+  },
+  "@media screen and (min-width: 768px) and (max-width: 992px)": {
+    width: "70vw",
+    left: "50%",
+    transform: "translateX(-50%)",
+    bottom: "40%",
+  },
+
+  "@media screen and (min-width: 992px) and (max-width: 1200px)": {
+    width: "70vw",
+    left: "50%",
+    transform: "translateX(-50%)",
+    top: "50%",
+  },
+
+  "@media screen and (min-width: 1200px)": {
+    // bottom: "40%",
+    top: "45%",
+    fontWeight: vars.fontWeights.normal,
   },
 });
 
@@ -90,8 +140,32 @@ export const carouselButton = style({
   bottom: "30%", // 根据需要调整
   left: "25%",
   transform: "translateX(-50%)",
-  "@media screen and (max-width: 768px)": {
+
+  "@media screen and (max-width: 576px)": {
+    bottom: "10%",
+    left: "50%",
+    transform: "translateX(-50%)",
+  },
+
+  "@media screen and (min-width: 576px) and (max-width: 768px)": {
     bottom: "20%", // 视口变小时调整位置
     left: "50%",
-    transform: "translateX(-50%)",}
+    transform: "translateX(-50%)",
+  },
+  "@media screen and (min-width: 768px) and (max-width: 992px)": {
+    left: "50%",
+    transform: "translateX(-50%)",
+
+    bottom: "30%",
+  },
+
+  "@media screen and (min-width: 992px) and (max-width: 1200px)": {
+    left: "50%",
+    transform: "translateX(-50%)",
+    bottom: "20%",
+  },
+
+  "@media screen and (min-width: 1200px)": {
+    bottom: "25%",
+  },
 });
